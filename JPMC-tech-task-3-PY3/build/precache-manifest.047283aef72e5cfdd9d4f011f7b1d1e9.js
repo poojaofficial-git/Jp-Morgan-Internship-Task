27a39a2a6ebb80285a48eeb7711c5faa5b22ed4e
poojaofficial-git <65713435+poojaofficial-git@users.noreply.github.com>
2020-06-27 12:48:03 +0530

    task files

---
+++ JPMC-tech-task-3-PY3/build/precache-manifest.047283aef72e5cfdd9d4f011f7b1d1e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "10fd38052bb4e79f5c09",
+    "url": "/static/css/main.afb9628a.chunk.css"
+  },
+  {
+    "revision": "10fd38052bb4e79f5c09",
+    "url": "/static/js/main.10fd3805.chunk.js"
+  },
+  {
+    "revision": "d59d071f45327e42b23f",
+    "url": "/static/css/1.7a2022fd.chunk.css"
+  },
+  {
+    "revision": "d59d071f45327e42b23f",
+    "url": "/static/js/1.d59d071f.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b01db9d8b8c0224e7a41479c8f9a23bb",
+    "url": "/index.html"
+  }
+];
